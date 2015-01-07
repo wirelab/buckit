b3efe5ee8f71824c041851368909bda578d66cd9
Andre Kramer <andre@panofy.com>
2015-01-07 21:49:56 +0100

    upgrade to aws-sdk 2.0 pre

--- lib/buckit/cli.rb
+++ lib/buckit/cli.rb
@@ -1,12 +1,12 @@
 require 'buckit/version'
 require 'buckit/exceptions'
-require 'aws/s3'
+require 'aws-sdk'
 require 'cmdparse'
+require "active_support/core_ext"
 
 module Buckit
   module CLI
     class BaseCmd < CmdParse::Command
-
       @has_prefix = false
 
       def has_options?
@@ -15,6 +15,10 @@ module Buckit
 
       def has_prefix?
         @has_prefix
+      end
+
+      def region
+        @region || 'eu-west-1'
       end
 
       def usage
@@ -33,28 +37,27 @@ module Buckit
       end
 
       def execute(args)
+        # Set default region, required
+        Aws.config[:region] = region
+
         # Connecting to amazon
-        s3 = AWS::S3.new
+        s3 = Aws::S3::Client.new
 
         # From the command line
-        key, file = args
-
-        # Parsing the bucket name
-        bucket = nil
-        bucket, key = key.split(':') if key
+        bucket, = args
 
         # Running our custom method inside of the command class, taking care
         # of the common errors here, saving duplications in each command;
         begin
-          run s3, bucket, key, file, args
-        rescue AWS::S3::Errors::AccessDenied
+          run s3, bucket, args
+        rescue Aws::S3::Errors::AccessDenied
           raise FailureFeedback.new("Access Denied")
-        rescue AWS::S3::Errors::NoSuchBucket
+        rescue Aws::S3::Errors::NoSuchBucket
           raise FailureFeedback.new("There's no bucket named `#{bucket}'")
-        rescue AWS::S3::Errors::NoSuchKey
-          raise FailureFeedback.new("There's no key named `#{key}' in the bucket `#{bucket}'")
-        rescue AWS::S3::Errors::Base => exc
-          raise FailureFeedback.new("Error: `#{exc.message}'")
+        rescue Aws::S3::Errors::BucketAlreadyOwnedByYou => message
+          raise FailureFeedback.new(message)
+        rescue Aws::S3::Errors => message
+          raise FailureFeedback.new("Error: `#{message}'")
         end
       end
     end
@@ -66,12 +69,8 @@ module Buckit
         @short_desc = "List all available buckets"
       end
 
-      def run s3, bucket, key, file, args
-        bucket_names = []
-        s3.buckets.each do |bkt|
-          bucket_names << "#{bkt.name}"
-        end
-        bucket_names.each { |bkt| puts bkt }
+      def run s3, bucket, args
+        s3.list_buckets.buckets.each { |b| puts b.name }
       end
     end
 
@@ -85,76 +84,80 @@ module Buckit
         @region = 'eu-west-1'
 
         self.options = CmdParse::OptionParserWrapper.new do |opt|
-          opt.on("-r", "--region", "specify a region, default eu-west-1") {|f|
-            @region = r
+          opt.on("-r", "--region REGION", "specify a region, default eu-west-1") {|region|
+            @region = region
           }
         end
       end
 
-      def run s3, bucket, key, file, args
+      def run s3, bucket, args
         raise WrongUsage.new(nil, "You need to supply a bucket name") if not bucket
 
         # Create bucket
-        AWS.config(region: @region)
-        begin
-          s3.buckets.create(bucket, acl: :public_read)
-        rescue AWS::S3::Errors::BucketAlreadyExists
-          raise FailureFeedback.new("Bucket `#{bucket}' already exists")
-        end
+        create_bucket(s3, bucket)
+        credentials = create_user(bucket)
+        create_policy(bucket)
+        add_cors(s3, bucket)
 
-        # Create IAM
+        puts "FOG_DIRECTORY=#{bucket} FOG_PROVIDER=AWS FOG_REGION=#{@region} ASSET_HOST=#{Aws::S3::Resource.new.bucket(bucket).url} AWS_ACCESS_KEY_ID=#{credentials.access_key_id} AWS_SECRET_ACCESS_KEY=#{credentials.secret_access_key}"
+      end
+
+      protected
+      def create_bucket s3, bucket
+        s3.create_bucket(
+          acl: "public-read",
+          bucket: bucket
+        )
+      end
+
+      def create_user bucket
         begin
-          user = AWS.iam.users.create(name)
-          access_key = user.access_keys.create
-          credentials = access_key.credentials
-        rescue AWS::Errors::ClientError
-          #TODO Rollback
+          iam = Aws::IAM::Client.new
+          iam.create_user(user_name: bucket, path: '/apps/')
+          credentials = iam.create_access_key(user_name: bucket)
+          credentials.first.access_key
+        rescue Aws::IAM::Errors::ServiceError
           raise FailureFeedback.new("Failed creating IAM")
         end
+      end
 
-        # Create Policy
+      def create_policy bucket
         begin
-          policy = AWS::IAM::Policy.new
-          policy.allow(
-            actions: ["s3:AbortMultipartUpload",
-            "s3:DeleteObject",
-            "s3:DeleteObjectVersion",
-            "s3:GetBucketAcl",
-            "s3:GetBucketLocation",
-            "s3:GetBucketLogging",
-            "s3:GetBucketNotification",
-            "s3:GetBucketVersioning",
-            "s3:GetBucketWebsite",
-            "s3:GetObject",
-            "s3:GetObjectAcl",
-            "s3:GetObjectTorrent",
-            "s3:GetObjectVersion",
-            "s3:GetObjectVersionAcl",
-            "s3:GetObjectVersionTorrent",
-            "s3:ListBucket",
-            "s3:ListBucketVersions",
-            "s3:PutBucketAcl",
-            "s3:PutBucketLogging",
-            "s3:PutBucketNotification",
-            "s3:PutBucketVersioning",
-            "s3:PutBucketWebsite",
-            "s3:PutLifecycleConfiguration",
-            "s3:PutObject",
-            "s3:PutObjectAcl",
-            "s3:PutObjectVersionAcl"],
-            resources: ["arn:aws:s3:::#{bucket}", "arn:aws:s3:::#{bucket}/*"]
-          )
-          policy_options = {}
-          policy_options[:user_name] = user.name
-          policy_options[:policy_name] = "buckit"
-          policy_options[:policy_document] = policy.to_json
-          AWS.iam.client.put_user_policy policy_options
-        rescue AWS::Errors::ClientError
-          #TODO Rollback
+          iam = Aws::IAM::Client.new
+          policy = {
+            "Statement" => [
+              {
+                "Action" => ["s3:AbortMultipartUpload", "s3:DeleteObject", "s3:DeleteObjectVersion", "s3:GetBucketAcl", "s3:GetBucketLocation", "s3:GetBucketLogging", "s3:GetBucketNotification", "s3:GetBucketVersioning", "s3:GetBucketWebsite", "s3:GetObject", "s3:GetObjectAcl", "s3:GetObjectTorrent", "s3:GetObjectVersion", "s3:GetObjectVersionAcl", "s3:GetObjectVersionTorrent", "s3:ListBucket", "s3:ListBucketVersions", "s3:PutBucketAcl", "s3:PutBucketLogging", "s3:PutBucketNotification", "s3:PutBucketVersioning", "s3:PutBucketWebsite", "s3:PutLifecycleConfiguration", "s3:PutObject", "s3:PutObjectAcl", "s3:PutObjectVersionAcl"],
+                "Effect" => "Allow",
+                "Resource" => ["arn:aws:s3:::#{bucket}", "arn:aws:s3:::#{bucket}/*"]
+              }
+            ]
+          }
+          iam.put_user_policy(user_name: bucket, policy_name: 'buckit', policy_document: policy.to_json)
+        rescue Aws::IAM::Errors::ServiceError
           raise FailureFeedback.new("Failed creating policy")
         end
+      end
 
-        puts "#{bucket}: AWS_ACCESS_KEY_ID=#{credentials[:access_key_id]} AWS_SECRET_ACCESS_KEY=#{credentials[:secret_access_key]}"
+      def add_cors s3, bucket
+        begin
+          s3.put_bucket_cors(
+            bucket: bucket,
+            cors_configuration: {
+              cors_rules: [
+                {
+                  allowed_headers: ['*'],
+                  allowed_methods: ["GET", "POST", "PUT"],
+                  allowed_origins: ['*'],
+                  expose_headers: [],
+                  max_age_seconds: 3000
+                },
+              ]
+            }
+          )
+        rescue Aws::S3::Errors::ServiceError
+          raise FailureFeedback.new("Failed creating CORS")
+        end
       end
     end
 
@@ -172,21 +175,30 @@ module Buckit
           opt.on("-f", "--force", "Clean the bucket then deletes it") {|f|
             @force = f
           }
+          opt.on("-r", "--region REGION", "specify a region, default eu-west-1. This must match the region in which the bucket resides.") {|region|
+            @region = region
+          }
         end
       end
 
-      def run s3, bucket, key, file, args
+      def run s3, bucket, args
         raise WrongUsage.new(nil, "You need to specify a bucket") if not bucket
 
         # Getting the bucket
-        bucket_obj = s3.buckets[bucket]
+        bucket_obj = Aws::S3::Resource.new(region: region).bucket(bucket)
 
+        begin
         # Do not kill buckets with content unless explicitly asked
         if not @force and bucket_obj.objects.count > 0
           raise FailureFeedback.new("Bucket `#{bucket}' still has files. Try with -f if you want to forcefully delete the bucket.")
         end
 
-        bucket_obj.delete!
+
+          bucket_obj.delete!
+          puts "#{bucket} has been deleted"
+        rescue Aws::S3::Errors::PermanentRedirect => message
+          raise FailureFeedback.new(message)
+        end
       end
     end
 
