22efe5bbe37c1f1fb932bdb9d809c5fcfbc04266
Andre Kramer <andre@panofy.com>
2015-01-07 22:45:26 +0100

    add options to delete user, policy and access keys while deleting a bucket

--- lib/buckit/cli/delete.rb
+++ lib/buckit/cli/delete.rb
@@ -24,6 +24,13 @@ module Buckit
           opt.on("-r", "--region REGION", "specify a region, default eu-west-1. This must match the region in which the bucket resides.") {|region|
             @region = region
           }
+          opt.on("-u", "--user", "USE WITH CARE! Removes the user with the name of the bucket, removes the policy named 'buckit' of this user, removes the users access_keys") {|user|
+            @remove_user = user
+          }
+          opt.on("-s", "--skip", "Skip trying to delete the buckit. Handy if you just want to remove the user") {|skip|
+            @skip = skip
+          }
+
         end
       end
 
@@ -38,8 +45,21 @@ module Buckit
           if not @force and bucket_obj.objects.count > 0
             raise FailureFeedback.new("Bucket `#{bucket}' still has files. Try with -f if you want to forcefully delete the bucket.")
           end
-          bucket_obj.delete!
-          puts "#{bucket} has been deleted"
+          unless @skip
+            bucket_obj.delete!
+            puts "bucket: #{bucket} has been deleted"
+          end
+          if @remove_user
+            iam = Aws::IAM::Resource.new region: region
+            user = iam.user(bucket)
+            user.policy("buckit").delete
+            puts "Policy deleted"
+            user.access_keys.each(&:delete)
+            puts "Access keys deleted"
+            user.delete
+            puts "User deleted"
+          end
+
         rescue Aws::S3::Errors::PermanentRedirect => message
           raise FailureFeedback.new(message)
         end
